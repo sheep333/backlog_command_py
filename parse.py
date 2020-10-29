7c47c384628da13587056b414d9c22c75c660bfc
sheep333 <yukiko.elike1.51@gmail.com>
2020-10-29 09:31:14 +0900

    fix output files

--- parse.py
+++ parse.py
@@ -3,13 +3,13 @@ from jinja2 import Environment, FileSystemLoader
 
 class Parse:
 
-    def create_html_file(self, template_name, file_name, data, output_path="./output/html/"):
+    def create_html_file(self, template_name, file_name, data, output_path="./output/"):
         """
         Jinja2のテンプレートエンジンを使って、HTMLファイルを作成する
         """
         env = Environment(loader=FileSystemLoader('./templates/'))
         template = env.get_template(template_name)
         output_html = template.render(data)
-        html_file = open("{output_path}/{file_name}", "w", encoding="utf-8")
+        html_file = open(f"{output_path}/{file_name}", "w", encoding="utf-8")
         html_file.write(output_html)
         html_file.close()
