14889fcdfa03ad2c890773cd5e4d0c56c652f71f
sheep333 <yukiko.elike1.51@gmail.com>
2021-01-24 04:11:54 +0900

    use common function and fix error message

--- command.py
+++ command.py
@@ -16,11 +16,11 @@ from monkey_patch import MySharedFile, MyUser, MyIssueAttachment, MyWikiAttachme
 
 SPACE_KEY = str(os.getenv('SPACE_KEY'))
 API_KEY = os.getenv('API_KEY')
-COUNT_PER_API = 100
+COUNT_PER_API = 2
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler())
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 config = BacklogComConfigure(space_key=SPACE_KEY, api_key=API_KEY)
 # TODO: パッチを当てたので、引数にoutput用のディレクトリを追加
@@ -36,6 +36,7 @@ class Command:
         if self.args.command != 'get_project_data':
             # get_project_data以外はそのままコマンド名を実行
             data = eval(f'self.{self.args.command}')()
+            print(data)
             self.parse.create_output_file(data, self.args)
         else:
             # get_project_dataは各種データをdictで取得
@@ -47,17 +48,17 @@ class Command:
                 'issues': issues
             }
             self.parse.create_html_file('issue_list.html', 'issue_list.html', data)
-            logger.info('Create issue_list.html')
+            logger.info('[INFO]Create issue_list.html')
             for issue in issues:
                 data['issue'] = issue
                 self.parse.create_html_file('issue_detail.html', f'issue_{issue["id"]}.html', data)
-                logger.info(f'Create issue_{issue["id"]}.html')
+                logger.info(f'[INFO]Create issue_{issue["id"]}.html')
             del data['issues'], data['issue']
 
             for wiki in wikis:
                 data['wiki'] = wiki
                 self.parse.create_html_file('wiki.html', f'wiki_{wiki["id"]}.html', data)
-                logger.info(f'Create wiki_{wiki["id"]}.html')
+                logger.info(f'[INFO]Create wiki_{wiki["id"]}.html')
 
     def _init_api(self):
         """
@@ -110,7 +111,11 @@ class Command:
         """
         レスポンスから必要なデータを取得しをdict型に変換
         """
-        return json.loads(response.content.decode('utf-8'))
+        logger.debug(f"[DEBUG]Response:{response.content}")
+        try:
+            return json.loads(response.content.decode('utf-8', errors='replace'))
+        except Exception:
+            logger.error("[ERROR]レスポンスがデコードできないため、スキップします。")
 
     def _add_user_icon(self, obj, users_icon):
         if 'createdUser' in obj and obj['createdUser']['id'] in users_icon:
@@ -132,67 +137,52 @@ class Command:
                     txt = re.sub(r'!\[image\]\[(.*)\]', file_image, txt, 1)
         return txt
 
+    def _check_status(self, response):
+        res = self._convert_res_to_dict(response)
+        if response.status_code == 200:
+            return res
+        else:
+            logger.error(res)
+            raise EnvironmentError("[ERROR]APIの情報がうまく取得できませんでした...")
+
     def get_users(self):
         response = self.user_api.get_user_list()
-        if response.status_code == 200:
-            return self._convert_res_to_dict(response)
-        else:
-            logger.error(self._convert_res_to_dict(response))
-            raise EnvironmentError("APIの情報がうまく取得できませんでした...")
+        return self._check_status(response)
 
     def get_user_icon(self, user_id, download_path=None):
         filepath, response = self.user_api.get_user_icon(user_id=user_id, download_path=download_path)
+        logger.info(f'[INFO]Saved user icon: {filepath}')
         if response.status_code == 200:
-            logger.info(f'Saved user icon: {filepath}')
             return filepath, response
         else:
-            logger.error(self._convert_res_to_dict(response))
-            raise EnvironmentError("APIの情報がうまく取得できませんでした...")
+            logger.warning(f"[WARNING]ユーザID: {user_id}のアイコンが取得できませんでした。")
 
     def get_issues(self):
         response = self.issue_api.get_issue_list()
-        if response.status_code == 200:
-            return self._convert_res_to_dict(response)
-        else:
-            logger.error(self._convert_res_to_dict(response))
-            raise EnvironmentError("APIの情報がうまく取得できませんでした...")
+        return self._check_status(response)
 
     def get_project(self):
         response = self.project_api.get_project(project_id_or_key=self.args.project)
-        if response.status_code == 200:
-            return self._convert_res_to_dict(response)
-        else:
-            logger.error(self._convert_res_to_dict(response))
-            raise EnvironmentError("APIの情報がうまく取得できませんでした...")
+        return self._check_status(response)
 
     def get_projects(self):
         response = self.project_api.get_project_list()
-        if response.status_code == 200:
-            return self._convert_res_to_dict(response)
-        else:
-            logger.error(self._convert_res_to_dict(response))
-            raise EnvironmentError("APIの情報がうまく取得できませんでした...")
+        return self._check_status(response)
 
     def get_project_icon(self):
         filepath, response = self.project_api.get_project_icon(project_id_or_key=self.args.project)
+        logger.info(f'[INFO]Saved project icon: {filepath}')
         if response.status_code == 200:
-            logger.info(f'Saved project icon: {filepath}')
             return filepath, response
         else:
-            logger.error(self._convert_res_to_dict(response))
-            raise EnvironmentError("APIの情報がうまく取得できませんでした...")
+            logger.warning(f"[WARNING]プロジェクトID: {self.args.project}のアイコンが取得できませんでした。")
 
     def get_project_issues(self):
         count_response = self.issue_api.count_issue(
             project_id=self.args.project
         )
-        if count_response.status_code == 200:
-            count = self._convert_res_to_dict(count_response)
-            # file数分ループ
-            file_count = math.ceil(count["count"] / COUNT_PER_API)
-        else:
-            logger.error(self._convert_res_to_dict(count_response))
-            raise EnvironmentError("APIの情報がうまく取得できませんでした...")
+        count_res = self._check_status(count_response)
+        file_count = math.ceil(count_res["count"] / COUNT_PER_API)
 
         issues = []
         for i in range(file_count):
@@ -201,51 +191,43 @@ class Command:
                 count=COUNT_PER_API,
                 offset=i * COUNT_PER_API
             )
-            if response.status_code == 200:
-                res = self._convert_res_to_dict(response)
-                issues += res
-            else:
-                logger.error(self._convert_res_to_dict(response))
-                raise EnvironmentError("APIの情報がうまく取得できませんでした...")
+            res = self._check_status(response)
+            issues += res
         return issues
 
     def get_project_users(self):
         response = self.project_api.get_project_user_list(project_id_or_key=self.args.project)
-        if response.status_code == 200:
-            project_users = self._convert_res_to_dict(response)
-            return project_users
-        else:
-            logger.error(self._convert_res_to_dict(response))
-            raise EnvironmentError("APIの情報がうまく取得できませんでした...")
+        return self._check_status(response)
 
     def get_issue_comments(self, issue_id):
         response = self.issue_comment_api.get_comment_list(
             issue_id_or_key=issue_id,
             count=COUNT_PER_API,
         )
-        if response.status_code == 200:
-            return self._convert_res_to_dict(response)
-        else:
-            logger.error(self._convert_res_to_dict(response))
-            raise EnvironmentError("APIの情報がうまく取得できませんでした...")
+        return self._check_status(response)
 
     def get_wiki_page_list(self):
         response = self.wiki_api.get_wiki_page_list(project_id_or_key=self.args.project)
         # 一覧に含まれるWikiにはcontentが含まれないため、改めて取得する
-        if response.status_code == 200:
-            wikis_list = self._convert_res_to_dict(response)
-            return [self._convert_res_to_dict(self.wiki_api.get_wiki_page(wiki['id'])) for wiki in wikis_list]
-        else:
-            logger.error(self._convert_res_to_dict(response))
-            raise EnvironmentError("APIの情報がうまく取得できませんでした...")
+        wikis_list = self._check_status(response)
+        return [self._convert_res_to_dict(self.wiki_api.get_wiki_page(wiki['id'])) for wiki in wikis_list]
+
+    def _add_file(self, func, data, data_type, **kwargs):
+        try:
+            logger.debug(f"[DEBUG]{data_type} ID: {data['id']}の処理を開始")
+            filepath, response = func(**kwargs)
+            logger.info(f'[INFO]Saved {data_type}({data["id"]}): {filepath}')
+            data['path'] = filepath
+        except Exception as e:
+            logger.error(f"{data_type} ID:{data['id']}が取得できませんでした。エラーメッセージ:{e}")
 
     def create_issue_template_data(self, issues, users_icon):
         # 課題とそのコメントのアイコン追加、マークダウンへの変換処理、添付ファイル取得
         for issue in issues:
-            logger.debug(f"課題ID: {issue['id']}の処理を開始")
+            logger.debug(f"[DEBUG]課題ID: {issue['id']}の処理を開始")
             self._add_user_icon(issue, users_icon)
 
-            path = f"issues/{issue['id']}/"
+            path = f"./output/issues/{issue['id']}/"
             os.makedirs(path, exist_ok=True)
 
             description = self._convert_image_link(issue['description'], path)
@@ -253,74 +235,56 @@ class Command:
 
             issue['comments'] = self.get_issue_comments(issue['id'])
             for comment in issue['comments']:
-                logger.debug(f"コメントID: {comment['id']}の処理を開始")
+                logger.debug(f"[DEBUG]コメントID: {comment['id']}の処理を開始")
                 self._add_user_icon(comment, users_icon)
                 content = self._convert_image_link(comment['content'], path)
                 comment['content'] = self.parse.to_markdown(content)
-            logger.info('Get comments')
+            logger.info(f'[INFO]Get comments of issue. IssueID: {issue["id"]}')
 
             for attachment in issue['attachments']:
-                try:
-                    logger.debug(f"アタッチメントID: {attachment['id']}の処理を開始")
-                    filepath, response = self.issue_attachment_api.get_issue_attachment(
-                        issue_id_or_key=issue['id'],
-                        attachment_id=attachment['id'],
-                        download_path=path
-                    )
-                    logger.info(f'Saved issue attachment: {filepath}')
-                    attachment['path'] = filepath
-                except Exception as e:
-                    logger.error(f"アタッチメントID:{attachment['id']}が取得できませんでした。エラーメッセージ:{e}")
+                param = {
+                    "issue_id_or_key": issue['id'],
+                    "attachment_id": attachment['id'],
+                    "download_path": path
+                }
+                self._add_file(self.issue_attachment_api.get_issue_attachment, attachment, 'issue_attachment', **param)
 
             for shared_file in issue['sharedFiles']:
-                logger.debug(f"共有ファイルID: {shared_file['id']}の処理を開始")
-                try:
-                    filepath, response = self.sharedfile_api.get_file(
-                        project_id_or_key=self.args.project,
-                        shared_file_id=shared_file['id'],
-                        download_path=path
-                    )
-                    logger.info(f'Saved issue sharefile: {filepath}')
-                    shared_file['path'] = filepath
-                except Exception as e:
-                    logger.error(f"共有ファイルID:{shared_file['id']}が取得できませんでした。エラーメッセージ:{e}")
+                param = {
+                    "project_id_or_key": self.args.project,
+                    "shared_file_id": shared_file['id'],
+                    "download_path": path
+                }
+                self._add_file(self.sharedfile_api.get_file, shared_file, 'issue_shared_file', **param)
         return issues
 
     def create_wiki_template_data(self, wikis, users_icon):
         # Wikiのマークダウンのアイコン追加、変換処理、添付ファイル取得
         for wiki in wikis:
-            logger.debug(f"WikiID: {wiki['id']}の処理を開始")
+            logger.debug(f"[DEBUG]WikiID: {wiki['id']}の処理を開始")
             self._add_user_icon(wiki, users_icon)
 
-            path = f"wikis/{wiki['id']}/"
+            path = f"./output/wikis/{wiki['id']}/"
             os.makedirs(path, exist_ok=True)
 
             content = self._convert_image_link(wiki['content'], path)
             wiki['content'] = self.parse.to_markdown(content)
 
             for attachment in wiki['attachments']:
-                try:
-                    filepath, response = self.wiki_attachment_api.get_wiki_page_attachment(
-                        wiki_id=wiki['id'],
-                        attachment_id=attachment['id'],
-                        download_path=path
-                    )
-                    logger.info(f'Saved wiki attachment: {filepath}')
-                    attachment['path'] = filepath
-                except Exception as e:
-                    logger.error(f"アタッチメントID:{attachment['id']}が取得できませんでした。エラーメッセージ:{e}")
+                param = {
+                    "wiki_id": wiki['id'],
+                    "attachment_id": attachment['id'],
+                    "download_path": path
+                }
+                self._add_file(self.wiki_attachment_api.get_wiki_page_attachment, attachment, 'wiki_attachment', **param)
 
             for shared_file in wiki['sharedFiles']:
-                try:
-                    filepath, response = self.sharedfile_api.get_file(
-                        project_id_or_key=self.args.project,
-                        shared_file_id=shared_file['id'],
-                        download_path=path
-                    )
-                    logger.info(f'Saved wiki sharefile: {filepath}')
-                    shared_file['path'] = filepath
-                except Exception as e:
-                    logger.error(f"共有ファイルID:{shared_file['id']}が取得できませんでした。エラーメッセージ:{e}")
+                param = {
+                    "project_id_or_key": self.args.project,
+                    "shared_file_id": shared_file['id'],
+                    "download_path": path
+                }
+                self._add_file(self.sharedfile_api.get_file, shared_file, 'wiki_shared_file', **param)
         return wikis
 
     def get_project_data(self):
@@ -334,13 +298,13 @@ class Command:
             raise AttributeError("プロジェクトIDを引数に指定してください。")
 
         project = self.get_project()
-        logger.info('Get project data')
+        logger.info('[INFO]Get project data')
         issues = self.get_project_issues()
-        logger.info('Get project issues')
+        logger.info('[INFO]Get project issues')
         wikis = self.get_wiki_page_list()
-        logger.info('Get project wiki')
+        logger.info('[INFO]Get project wiki')
         users = self.get_project_users()
-        logger.info('Get project users')
+        logger.info('[INFO]Get project users')
 
         # プロジェクトデータにアイコンを追加
         filepath, response = self.get_project_icon()
@@ -350,14 +314,14 @@ class Command:
         # ユーザアイコンの取得用dict作成
         users_icon = {}
         for user in users:
-            logger.debug(f"ユーザID: {user['id']} の処理を開始")
-            path = f"users/{user['id']}/"
+            logger.debug(f"[DEBUG]ユーザID: {user['id']} の処理を開始")
+            path = f"./output/users/{user['id']}/"
             os.makedirs(path, exist_ok=True)
             try:
                 filepath, response = self.get_user_icon(user['id'], download_path=path)
                 users_icon[user['id']] = filepath
             except Exception as e:
-                logger.warning(f"ユーザID:{user['id']}のユーザアイコンが取得できませんでした。エラーメッセージ:{e}")
+                logger.warning(f"[WARNING]ユーザID:{user['id']}のユーザアイコンが取得できませんでした。エラーメッセージ:{e}")
 
         issues = self.create_issue_template_data(issues, users_icon)
         wikis = self.create_wiki_template_data(wikis, users_icon)
